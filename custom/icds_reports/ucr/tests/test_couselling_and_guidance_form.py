42d9674d1c780e4c45224242430c32634df8ae93
Rohit Negi <rohit25.negi@gmail.com>
2020-08-17 18:00:37 +0530

    fixed the errors

--- custom/icds_reports/ucr/tests/test_couselling_and_guidance_form.py
+++ custom/icds_reports/ucr/tests/test_couselling_and_guidance_form.py
@@ -9,17 +9,18 @@ from custom.icds_reports.ucr.tests.test_base_form_ucr import BaseFormsTest
 @patch('corehq.apps.locations.ucr_expressions._get_location_type_name',
        lambda loc_id, context: 'awc')
 class TestCounselling_guidance_form(BaseFormsTest):
-    ucr_name = "static-icds-cas-dashboard_counselling_and_guidance_forms"
+    ucr_name = "static-icds-cas-static_dashboard_counselling_and_guidance_forms"
 
     def test_counselling_and_guidance_form(self):
         self._test_data_source_results(
-            'static_dashboard_counselling_and_guidance_forms',
-            [{
-                'admitted_in_school': None,
-                "doc_id": None,
-                "timeend": datetime.datetime(2019, 12, 9, 8, 23, 52, 511000),
-                'out_of_school': 'yes',
-                'person_case_id': 'dda684cc-f260-4a0e-b608-f22aca3bf467',
-                're_out_of_school': None,
-              }
-             ])
+            'counselling_and_guidance_form',
+            [{'ag_care_case_id': 'c03432da-411a-45b9-918a-27082ea4fb6e',
+              'counselling_session_conducted': 1,
+              'doc_id': None,
+              'repeat_iteration': 0,
+              'timeend': None},
+             {'ag_care_case_id': '139b95ea-008a-40ed-bb76-d5d6ecef12cd',
+              'counselling_session_conducted': 1,
+              'doc_id': None,
+              'repeat_iteration': 1,
+              'timeend': None}])
