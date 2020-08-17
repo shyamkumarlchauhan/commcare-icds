eefe9d302294271314eb41eba78559213084d677
V Sai Prakash <prakash@beehyv.com>
2020-08-17 23:20:12 +0530

    showing reassignment message

--- custom/icds_reports/static/js/directives/reassignment-message/reassignment-message.directive.js
+++ custom/icds_reports/static/js/directives/reassignment-message/reassignment-message.directive.js
@@ -1,3 +1,18 @@
+function ReassignmentMessageController($scope) {
+    var vm = this;
+    vm.$onChanges = function(){
+        if (vm.selectedLocation) {
+            vm.archived_on = vm.selectedLocation.archived_on;
+            vm.deprecated_to = vm.selectedLocation.deprecated_to;
+            vm.name = vm.selectedLocation.name;
+            vm.deprecates_at = vm.selectedLocation.deprecates_at;
+            vm.deprecates = vm.selectedLocation.deprecates;
+        }
+    }
+}
+
+ReassignmentMessageController.$inject = ['$scope',]
+
 window.angular.module('icdsApp').component("reassignmentMessage", {
     bindings: {
         selectedLocation: '<',
@@ -6,4 +21,5 @@ window.angular.module('icdsApp').component("reassignmentMessage", {
     templateUrl: ['templateProviderService', function (templateProviderService) {
         return templateProviderService.getTemplate('reassignment-message');
     }],
+    controller: ReassignmentMessageController,
 });
