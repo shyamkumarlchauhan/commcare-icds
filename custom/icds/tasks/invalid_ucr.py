b23f952e9f244d4d889c442f1cf3c677f022c23b
Simon Kelly <skelly@dimagi.com>
2020-08-18 16:36:29 +0200

    rename task

--- custom/icds/tasks/invalid_ucr.py
+++ custom/icds/tasks/invalid_ucr.py
@@ -9,6 +9,6 @@ from corehq.util.celery_utils import periodic_task_on_envs
 
 
 @periodic_task_on_envs(settings.ICDS_ENVS, run_every=crontab(hour=21))
-def delete_old_sms_events():
+def delete_old_ucr_logs():
     end_date = datetime.utcnow() - relativedelta(months=3)
     InvalidUCRData.objects.filter(date_created__lt=end_date).delete()
