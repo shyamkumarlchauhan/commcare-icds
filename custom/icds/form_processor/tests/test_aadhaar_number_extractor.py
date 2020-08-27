a625405ec96d53c211d138d0f51402fb7d490ee1
mkangia <mkangia@dimagi.com>
2020-08-27 14:21:29 +0530

    fix tests

--- custom/icds/form_processor/tests/test_aadhaar_number_extractor.py
+++ custom/icds/form_processor/tests/test_aadhaar_number_extractor.py
@@ -1,4 +1,3 @@
-import re
 from unittest import TestCase
 
 from custom.icds.form_processor.consts import AADHAAR_XFORM_SUBMISSION_PATTERNS
@@ -6,9 +5,6 @@ from custom.icds.form_processor.consts import AADHAAR_XFORM_SUBMISSION_PATTERNS
 
 class TestAadhaarPattern(TestCase):
     def test_aadhaar_number_pattern(self):
-        aadhaar_number_pattern = AADHAAR_XFORM_SUBMISSION_PATTERNS[0]
-        matches = re.findall(aadhaar_number_pattern, "<aadhar_number>123456789012</aadhar_number>")
-        self.assertEqual(matches, ['123456789012'])
-        matches = re.findall(aadhaar_number_pattern, "<aadhar_number>12345678901</aadhar_number>")
-        self.assertEqual(matches, [])
-
+        aadhaar_number_pattern = AADHAAR_XFORM_SUBMISSION_PATTERNS['aadhar_number']
+        self.assertTrue(aadhaar_number_pattern.match('123456789012'))
+        self.assertFalse(aadhaar_number_pattern.match('12345678901'))
