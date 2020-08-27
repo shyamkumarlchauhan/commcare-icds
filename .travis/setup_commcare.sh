502d0aa5a2d2468dd559a3a837db3c533b3d7077
Simon Kelly <skelly@dimagi.com>
2020-08-27 08:39:12 +0200

    check actual var

--- .travis/setup_commcare.sh
+++ .travis/setup_commcare.sh
@@ -3,11 +3,11 @@ set -e
 
 echo "CommCare Setup"
 hq_branch=$(git ls-remote --heads https://github.com/dimagi/commcare-hq.git $TRAVIS_PULL_REQUEST_BRANCH)
-if [ ! -z "hq_branch" ]; then
+if [ -z "$hq_branch" ]; then
+    echo "Using 'master' branch for commcare-hq clone."
+else
     echo "Using '$TRAVIS_PULL_REQUEST_BRANCH' for commcare-hq clone."
     GIT_BRANCH_ARGS="--branch $TRAVIS_PULL_REQUEST_BRANCH --single-branch"
-else
-    echo "Using 'master' branch for commcare-hq clone."
 fi
 
 CLONE_COMMAND="git clone https://github.com/dimagi/commcare-hq.git $GIT_BRANCH_ARGS $HOME/commcare --depth=1"
