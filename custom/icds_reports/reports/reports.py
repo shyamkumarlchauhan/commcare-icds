e56b2521399996ce0051cbf570885603e60a565a
Rohit Negi <rohit25.negi@gmail.com>
2020-08-28 12:36:46 +0530

    use it in  MPR report

--- custom/icds_reports/reports/reports.py
+++ custom/icds_reports/reports/reports.py
@@ -9,7 +9,8 @@ from custom.icds_reports.asr_sqldata import ASRIdentification, ASROperationaliza
 from custom.icds_reports.filters import ICDSMonthFilter, IcdsLocationFilter, IcdsRestrictedLocationFilter
 from custom.icds_reports.mpr_sqldata import MPRIdentification, MPRSectors, MPRPopulation, MPRBirthsAndDeaths, \
     MPRAWCDetails, MPRSupplementaryNutrition, MPRUsingSalt, MPRProgrammeCoverage, MPRPreschoolEducation, \
-    MPRGrowthMonitoring, MPRImmunizationCoverage, MPRVhnd, MPRReferralServices, MPRMonitoring, MPRMonitoringBeta, MPRVhndBeta
+    MPRGrowthMonitoring, MPRImmunizationCoverage, MPRVhnd, MPRReferralServices, MPRMonitoring, MPRMonitoringBeta, MPRVhndBeta, \
+    MPRBirthsAndDeathsBeta
 from custom.icds_reports.mpr_sqldata import MPROperationalization
 from custom.icds_reports.reports import IcdsBaseReport
 
@@ -30,7 +31,7 @@ class MPRReport(IcdsBaseReport):
             MPROperationalization,
             MPRSectors,
             MPRPopulation,
-            MPRBirthsAndDeaths,
+            MPRBirthsAndDeaths if not self.icds_pre_release_features() else MPRBirthsAndDeathsBeta,
             MPRAWCDetails,
             MPRSupplementaryNutrition,
             MPRUsingSalt,
