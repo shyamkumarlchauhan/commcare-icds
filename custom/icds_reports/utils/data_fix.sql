c89bc0a0a55c1e5214049c840d89490d8e5fc908
Shubham <shubhambansal17@hotmail.com>
2020-09-08 12:08:33 +0530

    Update data_fix.sql

--- custom/icds_reports/utils/data_fix.sql
+++ custom/icds_reports/utils/data_fix.sql
@@ -2,4 +2,5 @@ UPDATE icds_reports_aggregateinactiveaww ac
     SET awc_site_code = loc.awc_site_code
     FROM awc_location_local loc
     WHERE loc.doc_id = ac.awc_id
+    AND ac.awc_site_code LIKE 'awc%'
     AND loc.aggregation_level = 5;
