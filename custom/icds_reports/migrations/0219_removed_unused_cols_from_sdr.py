61535af06acaf797e66b15d936c894ebf53ed67b
Rohit Negi <rohit25.negi@gmail.com>
2020-10-29 13:29:52 +0530

    fixed migration

--- custom/icds_reports/migrations/0219_removed_unused_cols_from_sdr.py
+++ custom/icds_reports/migrations/0219_removed_unused_cols_from_sdr.py
@@ -6,7 +6,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('icds_reports', '0217_rebuild_db_views'),
+        ('icds_reports', '0218_removed_unused_cols_person_tb'),
     ]
 
     operations = [
