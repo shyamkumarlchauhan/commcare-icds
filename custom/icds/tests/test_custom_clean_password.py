70aa604a511525dc80cff5aec0749cf823090ab1
mkangia <mkangia@dimagi.com>
2020-11-17 12:38:05 +0530

    simplify tests

--- custom/icds/tests/test_custom_clean_password.py
+++ custom/icds/tests/test_custom_clean_password.py
@@ -1,52 +1,42 @@
 from django.test import SimpleTestCase
 
 import corehq.apps.domain.forms as forms
-from corehq.util.test_utils import generate_cases
 
-from custom.icds.commcare_extensions import password_strength
-
-# 1 Special Character, 1 Number, 1 Capital Letter with the length of Minimum 8
-tests = [
-    ('abcdefg', -2),
-    ('ABCDEFG', -1),
-    ('1234567', -1),
-    ('!@#$%^&', -1),
-    ('abcdef*', -1),
-    ('Abcdef*', 0),
-    ('0bcdef*', 0),
-    ('Ab0000*', 1),
-    ('AB0000*', 1),
-
-    ('abcdefgh', -1),
-    ('ABCDEFGH', 0),
-    ('12345678', 0),
-    ('!@#$%^&*', 0),
-    ('abcdefg*', 0),
-    ('Abcdefg*', 1),
-    ('0bcdefg*', 1),
-    ('åb0000g*', 1),  # lowercase unicode character does not count
-    ('Ab0000g*', 2),
-    ('AB0000G*', 2),
-    ('Åb0000g*', 2),  # uppercase unicode character
-]
+# custom.icds.commcare_extensions._complex_password
+passwords = {
+    'invalid': [
+        'abcdefg',
+        'ABCDEFG',
+        '1234567',
+        '!@$%^&',
+        'abcdef*',
+        'Abcdef*',
+        '0bcdef*',
+        'Ab0000*',
+        'AB0000*',
+        'abcdefgh',
+        'ABCDEFGH',
+        '12345678',
+        '!@$%^&*',
+        'abcdefg*',
+        'Abcdefg*',
+        '0bcdefg*',
+        'åb0000g*',  # lowercase unicode character does not count
+    ],
+    'valid': [
+        'Ab0000g*',
+        'AB0000G*',
+        'Åb0000g*',  # uppercase unicode character
+    ]
+}
 
 
 class TestLegacyPassword(SimpleTestCase):
-    pass
+    def test_invalid_passwords(self):
+        for password in passwords['invalid']:
+            with self.assertRaises(forms.forms.ValidationError):
+                forms.clean_password(password)
 
-
-@generate_cases(tests, TestLegacyPassword)
-def test_legacy_get_password_strength_score(self, password, expected_score):
-    strength = password_strength(password)
-    self.assertEqual(strength['score'], expected_score)
-
-
-@generate_cases([t[:1] for t in tests if t[1] < 2], TestLegacyPassword)
-def test_legacy_clean_password_failures(self, password):
-    with self.assertRaises(forms.forms.ValidationError):
-        forms.clean_password(password)
-
-
-@generate_cases([t[:1] for t in tests if t[1] >= 2], TestLegacyPassword)
-def test_legacy_clean_password_pass(self, password):
-    self.assertEqual(forms.clean_password(password), password)
+    def test_valid_passwords(self):
+        for password in passwords['valid']:
+            forms.clean_password(password)
