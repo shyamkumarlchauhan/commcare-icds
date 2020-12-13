6ea45d1aa3cf8ea37d5aa5bb094965fa892423ce
Rohit Negi <rohit25.negi@gmail.com>
2020-12-14 01:34:05 +0530

    fixed stunting test case

--- custom/icds_reports/tests/agg_tests/reports/test_prevalence_of_undernutrition.py
+++ custom/icds_reports/tests/agg_tests/reports/test_prevalence_of_undernutrition.py
@@ -113,8 +113,8 @@ class TestPrevalenceOfUndernutrition(TestCase):
         self.assertListEqual(
             data['rightLegend']['extended_info'],
             [
-                {'indicator': 'Total Children (0 - 5 years) weighed in given month:', 'value': '3,475'},
-                {'indicator': 'Number of children unweighed (0 - 5 years):', 'value': '1,480'},
+                {'indicator': 'Total Children (0 - 5 years) weighed in given month:', 'value': '695'},
+                {'indicator': 'Number of children unweighed (0 - 5 years):', 'value': '294'},
                 {'indicator': '% Severely Underweight (0 - 5 years):', 'value': '2.88%'},
                 {'indicator': '% Moderately Underweight (0 - 5 years):', 'value': '18.56%'},
                 {'indicator': '% Normal (0 - 5 years):', 'value': '78.56%'}
